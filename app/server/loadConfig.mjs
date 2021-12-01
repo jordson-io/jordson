3cdebd4a3f6d49201c83d71f35151eec027639e7
Andre LECLERCQ <andreleclercqpro@gmail.com>
2021-12-01 14:27:50 +0100

    refacto loadConfig

--- app/server/loadConfig.mjs
+++ app/server/loadConfig.mjs
@@ -21,7 +21,6 @@ export const loadConfig = function() {
 
   const fileName = fs.existsSync('./jordson.local.json') ? './jordson.local.json' : './jordson.json'
   return JSON.parse(fs.readFileSync(fileName, "utf-8"))
-
 };
 
 export default loadConfig;
